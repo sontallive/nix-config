710f0f4c147a42bb0a684e0d002da6a793982920
sontallive <418773551@qq.com>
2025-03-03 22:11:23 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,125 @@
+{
+  description = "Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  let
+    configuration = { pkgs, config, ... }: {
+      # 基础系统配置
+      nixpkgs = {
+        config.allowUnfree = true;
+        hostPlatform = "aarch64-darwin";
+      };
+
+      nix.settings.experimental-features = "nix-command flakes";
+      system.stateVersion = 6;
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # 系统包管理
+      environment.systemPackages = with pkgs; [
+        # 开发工具
+        neovim
+        vscode
+        # cursor
+	bun
+
+        # 终端工具
+        alacritty
+        tmux
+        mkalias
+        ripgrep
+        eza
+
+        # 字体
+        nerd-fonts.roboto-mono
+        nerd-fonts.jetbrains-mono
+
+        # 应用程序
+        obsidian
+        google-chrome
+        windsurf
+        code-cursor
+        apktool
+      ];
+
+      # Add these programs to the system PATH
+      environment.pathsToLink = [ "/bin" "/share/man" ];
+      
+      # 配置字体 - 使用正确的选项
+      fonts.packages = with pkgs; [
+        nerd-fonts.roboto-mono
+        nerd-fonts.jetbrains-mono
+      ];
+      
+      users.users.sontal.home = "/Users/sontal";
+
+      # macOS 系统偏好设置
+      system.defaults = {
+        dock = {
+          autohide = false;
+          # Make sure this setting is applied by using wipeAndPrepare
+          persistent-apps = [
+            "${pkgs.alacritty}/Applications/Alacritty.app"
+            "${pkgs.obsidian}/Applications/Obsidian.app"
+            # "${pkgs.vscode}/Applications/Visual Studio Code.app"
+            "${pkgs.code-cursor}/Applications/Cursor.app"
+            "${pkgs.google-chrome}/Applications/Google Chrome.app"
+            "/System/Applications/Mail.app"
+            "/System/Applications/Launchpad.app"
+          ];
+        };
+        finder.FXPreferredViewStyle = "clmv";
+      };
+
+      # Applications 文件夹管理
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        };
+      in
+        pkgs.lib.mkForce ''
+          # 设置应用程序链接
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read -r src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
+    };
+  in
+  {
+    darwinConfigurations."Macbook-Pro" = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [ 
+        configuration
+        home-manager.darwinModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.sontal = import ./home/sontal.nix;
+            # Add this to ensure Home Manager activates properly
+            extraSpecialArgs = { inherit inputs; };
+          };
+        }
+      ];
+    };
+  };
+}
---
+++ home/sontal.nix
@@ -0,0 +1,107 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager 需要一个版本号
+  home.stateVersion = "23.11";
+  
+  # 让 Home Manager 安装和管理自身
+  programs.home-manager.enable = true;
+  
+
+  home.username = "sontal";
+  home.homeDirectory = "/Users/sontal";
+
+  # 用户特定的包
+  home.packages = with pkgs; [
+    # 开发工具
+    git
+    gh
+    
+    # CLI 工具
+    ripgrep
+    eza
+    
+    # 其他工具
+    # ...
+  ];
+
+  # 配置程序
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    # 添加您的 zsh 配置
+    
+    # Add eza aliases to zsh
+    shellAliases = {
+      ls = "eza";
+      ll = "eza -l";
+      la = "eza -la";
+    };
+  };
+
+  # Git 配置
+  programs.git = {
+    enable = true;
+    userName = "sontallive";
+    userEmail = "418773551@qq.com";
+    # 其他 Git 配置
+  };
+
+  # Bash configuration (if you use it)
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ls = "eza";
+      ll = "eza -l";
+      la = "eza -la";
+    };
+  };
+
+  # Fish shell configuration
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ls = "eza";
+      ll = "eza -l";
+      la = "eza -la";
+    };
+    
+    # Make sure Fish can find system-installed Nix packages
+    shellInit = ''
+      # Add system Nix paths to Fish
+      set -gx PATH /run/current-system/sw/bin $PATH
+      
+      # Also include user profile for other Nix packages
+      set -gx PATH $HOME/.nix-profile/bin $PATH
+    '';
+    
+    # Fish functions if needed
+    functions = {
+      # Example function
+      # fish_greeting = "echo Welcome to Fish, $(whoami)!";
+    };
+    
+    # Add the foreign-env plugin to source bash files
+    plugins = [
+      {
+        name = "foreign-env";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-foreign-env";
+          rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+          sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+        };
+      }
+    ];
+  };
+
+  # 其他程序配置
+  # ...
+
+  # 文件配置
+  home.file = {
+    ".config/alacritty/alacritty.toml".source = ./dotfiles/alacritty.toml;
+  };
+}
